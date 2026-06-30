5ba7221b9471555b728f7f4238925bfec520d87d
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-06-30 17:26:09 -0500

    Adding initial files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FGC_Stat_Analyzer_wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
