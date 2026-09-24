64e66ea8858aaa632eaa44c8cff3939e62a5024f
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-09-24 13:56:24 -0500

    Fixed hyperlink functionality

--- Views/ChildWindows/LandingPage.xaml.cs
+++ Views/ChildWindows/LandingPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace FGC_Stat_Analyzer_wpf.Views.ChildWindows
 {
@@ -19,6 +21,17 @@ namespace FGC_Stat_Analyzer_wpf.Views.ChildWindows
         private void OAuthButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+
+            e.Handled = true;
         }
     }
 }
